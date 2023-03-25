3bd9ed204848bd92813b98de52294d248b25b482
T-Nabeel <nmtn3030@gmail.com>
2023-03-25 17:07:31 +0530

    Added About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>
-    <h1>This is an about page Homie</h1>
-  </div>
+  <main class="container">
+
+    <section class="intro-section">
+      <div class="intro-section__text">
+        <h1 class="intro-section__text__heading-one">I am an <span>Artist</span></h1>
+        <h2 class="intro-section__text__heading-two">who is passionate for</h2>
+        <h2 class="intro-section__text__heading-two">Design & Coding</h2>
+      </div>
+      <div class="intro-section__img">
+        <img src="../assets/images/about-intro.gif" alt="" />
+      </div>
+    </section>
+
+    <section class="occupation">
+      <div class="occupation__bg-gradient"></div>
+      <div class="occupation__para">
+        <h1>Currently I’m working as a UI/UX Designer and a Frontend Developer</h1>
+      </div>
+    </section>
+
+    <section class="scope-of-work">
+      <div class="scope-of-work__bg-gradient"></div>
+      <div class="scope-of-work__heading">
+        <h1>My Scope of work</h1>
+      </div>
+      <div class="scope-of-work__items">
+
+        <div class="scope-of-work__items__item">
+          <h2 class="scope-of-work__items__item--heading">Graphic Design</h2>
+          <p class="scope-of-work__items__item--para">
+            I create simple and attractive graphics that clearly convey your brand's message with impact.
+          </p>
+        </div>
+
+        <div class="scope-of-work__items__item">
+          <h2 class="scope-of-work__items__item--heading">UI/UX Design</h2>
+          <p class="scope-of-work__items__item--para">
+            I design user-friendly interfaces that improve the user experience and encourage engagement.
+          </p>
+        </div>
+
+        <div class="scope-of-work__items__item">
+          <h2 class="scope-of-work__items__item--heading">Development</h2>
+          <p class="scope-of-work__items__item--para">
+            I develop websites that effectively meet your business goals, using my passion for problem-solving.
+          </p>
+        </div>
+      </div>
+    </section>
+
+  </main>
 </template>
 
 <style>
-
 </style>
