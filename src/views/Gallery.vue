b1625a55081cefe9e344fc2a4096a697945ce0fc
T-Nabeel <nmtn3030@gmail.com>
2023-04-03 21:27:33 +0530

    Added Gallery page

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>
-    <h1>This is Galleryyyyyyyy</h1>
-  </div>
+  <main class="container">
+    <section class="gallery">
+      <div class="gallery__heading">
+        <h1>Few of my Artworks</h1>
+        <div class="gallery__heading__bg-gradient"></div>
+      </div>
+      <div class="gallery__images">
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-1.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-4.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-7.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-2.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-5.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-8.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-10.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-3.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-6.jpg" alt="gallery image" />
+        </div>
+        <div class="gallery__images__image">
+          <img src="../assets/images/gallery-img-9.jpg" alt="gallery image" />
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style>
-
 </style>
